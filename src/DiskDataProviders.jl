3c56b000f3ad8556c94597cd4aa1e2f185543628
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2019-08-14 16:14:33 +0800

    Integration with MLDataUtils

--- src/DiskDataProviders.jl
+++ src/DiskDataProviders.jl
@@ -1,33 +1,46 @@
 module DiskDataProviders
-using MLDataUtils, DataFrames, Dates, Serialization
+using MLDataUtils, LearnBase, DataFrames, Dates, Serialization
 
 import Base.Threads: nthreads, threadid, @spawn, SpinLock
 
-export DiskDataProvider2, label2filedict, start_reading, stop
+export DiskDataProvider, label2filedict, start_reading, stop!
 
 Serialization.serialize(filename::AbstractString, data) = open(f->serialize(f, data), filename, "w")
 Serialization.deserialize(filename) = open(f->deserialize(f), filename)
 
-Base.@kwdef mutable struct DiskDataProvider2{XT,YT}
-    length     ::Int = typemax(Int)
+Base.@kwdef mutable struct DiskDataProvider{XT,YT}
+    batchsize  ::Int           = 8
+    length     ::Int           = typemax(Int)
     labels     ::Vector{YT}
+    ulabels    ::Vector{YT}    = unique(labels)
     label2files::Dict{YT,Vector{String}}
     queue_full ::Threads.Event = Threads.Event()
     queue      ::Vector{Tuple{XT,YT}}
-    label_iterator
-    label_iterator_state = nothing
-    position   ::Int = 1
-    reading    ::Bool = false
-    queuelock  ::SpinLock = SpinLock()
+    label_iterator             = Iterators.cycle(unique(labels))
+    label_iterator_state       = nothing
+    position   ::Int           = 1
+    reading    ::Bool          = false
+    queuelock  ::SpinLock      = SpinLock()
+    x_batch    ::Array{Float32,4}
+    y_batch    ::Vector{YT}
 end
 
-function DiskDataProvider2{XT,YT}(queuelength::Int; kwargs...) where {XT,YT}
-    DiskDataProvider2{XT,YT}(; queue=Vector{Tuple{XT,YT}}(undef,queuelength), kwargs...)
+function DiskDataProvider{XT,YT}(xsize, batchsize, queuelength::Int; kwargs...) where {XT,YT}
+    queue   = Vector{Tuple{XT,YT}}(undef, queuelength)
+    x_batch = Array{Float32,4}(undef, xsize..., batchsize)
+    y_batch = Vector{YT}(undef, batchsize)
+    DiskDataProvider{XT,YT}(;
+        queue   = queue,
+        batchsize = batchsize,
+        x_batch = x_batch,
+        y_batch = y_batch,
+        kwargs...)
 end
 
-nclasses(d::DiskDataProvider2) = length(unique(d.labels))
-
-stop(d) = (d.reading = false)
+MLDataUtils.nobs(d::DiskDataProvider)  = length(d.queue)
+nclasses(d::DiskDataProvider)  = length(d.ulabels)
+stop!(d)                       = (d.reading = false)
+Base.wait(d::DiskDataProvider) = wait(d.queue_full)
 
 macro withlock(l, ex)
     quote
@@ -38,7 +51,7 @@ macro withlock(l, ex)
     end
 end
 
-withlock(f, l::DiskDataProvider2) = withlock(f, l.queuelock)
+withlock(f, l::DiskDataProvider) = withlock(f, l.queuelock)
 
 function withlock(f, l::Base.AbstractLock)
     lock(l)
@@ -49,8 +62,7 @@ function withlock(f, l::Base.AbstractLock)
     end
 end
 
-
-function populate_queue(d::DiskDataProvider2)
+function populate_queue(d::DiskDataProvider)
     while d.reading
         y = sample_label(d)
         x = sample_input(d,y)
@@ -67,13 +79,11 @@ function populate_queue(d::DiskDataProvider2)
     @info "Stopped reading"
 end
 
-Base.wait(d::DiskDataProvider2) = wait(d.queue_full)
 
-function start_reading(d::DiskDataProvider2)
+function start_reading(d::DiskDataProvider)
     d.reading = true
     task = @spawn populate_queue(d)
-    @info "Populating queue continuosly. Call `stop(d)` to stop reading`. Call `wait(d)` to be notified when the queue is fully populated."
-
+    @info "Populating queue continuosly. Call `stop!(d)` to stop reading`. Call `wait(d)` to be notified when the queue is fully populated."
     task
 end
 
@@ -101,16 +111,55 @@ function label2filedict(labels, files)
     label2files = Dict(Pair.(ulabels, ufiles))
 end
 
-function Base.iterate(d::DiskDataProvider2, state=0)
+function Base.iterate(d::DiskDataProvider, state=0)
     state == length(d) && return nothing
-     (sample_random_datapoint(d),state+1)
+    (sample_random_datapoint(d),state+1)
 end
-Base.length(d::DiskDataProvider2) = d.length
+Base.length(d::DiskDataProvider) = d.length
+
+# struct DiskBatches{T,N,XT,YT}
+#     d::DiskDataProvider{XT,YT}
+#     x::Array{T,N}
+#     y::Vector{YT}
+# end
+#
+#
+# function Base.iterate(db::DiskBatches, state=0)
+#     for (i,(x,y)) = enumerate(Iterators.take(db.d, size(db.x, 4)))
+#         db.x[:,:,1,i] .= x
+#         db.y[i] = y
+#     end
+#     ((db.x, db.y), state+1)
+# end
+# Base.length(d::DiskBatches) = typemax(Int)
+
 
 function sample_random_datapoint(d)
     wait(d.queue_full)
     i = rand(1:length(d.queue))
     d.queue[i]
 end
+
+
+function populate_batch(d,inds)
+    for (i,j) in enumerate(inds)
+        x,y = d.queue[j]
+        d.x_batch[:,:,:,i] .= x
+        d.y_batch[i] = y
+    end
+    (d.x_batch, d.y_batch)
+end
+
+function Base.iterate(ds::DataSubset{<:DiskDataProvider}, state=nothing)
+    inds = ds.indices
+    d = ds.data
+    populate_batch(d,inds)
+end
+
+MLDataUtils.batchview(d::DiskDataProvider) = batchview(d, size=d.batchsize)
+
+LearnBase.getobs(d::DiskDataProvider, inds) = populate_batch(d,inds)
+LearnBase.nobs(d::DiskDataProvider) = length(d.queue)
+LearnBase.datasubset(d::DiskDataProvider, inds, ::ObsDim.Undefined) = populate_batch(d,inds)
 
 end
